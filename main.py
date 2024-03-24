35cb7b00fb2a01ae82cc5a4f9beb83c8954a00dd
santoshi-1 <karuturisantoshi@gmail.com>
2024-03-23 23:27:43 -0400

    data preprocessing

--- main.py
+++ main.py
@@ -1,38 +1,46 @@
 import pandas as pd
-import numpy as np
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
-import string
-import nltk
 import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from gensim.models.phrases import Phrases, Phraser
 
+nltk.download('punkt')
 nltk.download('stopwords')
-nltk.download('wordnet')
+
+def preprocess_text(text):
+    # Convert to lowercase
+    text = text.lower()
+    
+    # Remove punctuation
+    text = re.sub(r'[^a-zA-Z\s]', '', text)
+    
+    # Tokenization
+    tokens = word_tokenize(text)
+    
+    # Remove stopwords
+    stop_words = set(stopwords.words('english'))
+    tokens = [word for word in tokens if word not in stop_words]
+    
+    # Generate bigrams using Phrases model
+    phrases = Phrases([tokens], min_count=1, threshold=1)
+    bigram = Phraser(phrases)
+    tokens = bigram[tokens]
+    
+    # Join tokens back into a string
+    processed_text = ' '.join(tokens)
+    
+    return processed_text
 
 # Load data
 df = pd.read_csv('./electronics_sample.csv')
 df.drop('summary', axis=1, inplace=True)
 df.dropna(subset=['reviewText'], inplace=True)
+#df = df[:19700]
 
-def preprocess_text(text):
-    
-    text = text.lower()
-    
-    text = re.sub(r'[^a-z\s]', '', text)  
-    
-    tokens = text.split()
-    
-    stop_words = set(stopwords.words('english'))
-    tokens = [word for word in tokens if word not in stop_words]
-    
-    lemmatizer = WordNetLemmatizer()
-    tokens = [lemmatizer.lemmatize(word) for word in tokens]
-    
-    text = ' '.join(tokens)
-    return text
-
-# Apply preprocessing to reviewText and summary
+# Apply preprocessing to reviewText
 df['processed_reviewText'] = df['reviewText'].apply(preprocess_text)
+processed_df = df[['processed_reviewText']]
 
-# Save the DataFrame to an Excel file
-df.to_csv('./processed_electronics_reviews.csv', index=False)
+# Save the DataFrame to a new CSV file
+processed_df.to_csv('processed_reviews.csv', index=False)
