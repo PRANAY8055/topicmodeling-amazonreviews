1f24c1c10dfa499e073f0232b4bfd9456277e4ac
santoshi-1 <karuturisantoshi@gmail.com>
2024-03-24 17:54:51 -0400

    data preprocessing

--- main.py
+++ main.py
@@ -1,46 +1,50 @@
 import pandas as pd
-import re
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-from gensim.models.phrases import Phrases, Phraser
+from nltk.stem import WordNetLemmatizer
+import string
 
+# Download NLTK resources
 nltk.download('punkt')
 nltk.download('stopwords')
+nltk.download('wordnet')
 
+# Load your dataset
+df = pd.read_csv('./electronics_sample.csv')
+df.drop('summary', axis=1, inplace=True)
+df.dropna(subset=['reviewText'], inplace=True)
+
+
+# Function to preprocess text
 def preprocess_text(text):
-    # Convert to lowercase
+    # Convert text to lowercase
     text = text.lower()
     
     # Remove punctuation
-    text = re.sub(r'[^a-zA-Z\s]', '', text)
+    text = text.translate(str.maketrans('', '', string.punctuation))
     
     # Tokenization
     tokens = word_tokenize(text)
     
     # Remove stopwords
     stop_words = set(stopwords.words('english'))
-    tokens = [word for word in tokens if word not in stop_words]
+    tokens = [token for token in tokens if token not in stop_words]
     
-    # Generate bigrams using Phrases model
-    phrases = Phrases([tokens], min_count=1, threshold=1)
-    bigram = Phraser(phrases)
-    tokens = bigram[tokens]
+    # Lemmatization
+    lemmatizer = WordNetLemmatizer()
+    tokens = [lemmatizer.lemmatize(token) for token in tokens]
     
     # Join tokens back into a string
     processed_text = ' '.join(tokens)
     
     return processed_text
 
-# Load data
-df = pd.read_csv('./electronics_sample.csv')
-df.drop('summary', axis=1, inplace=True)
-df.dropna(subset=['reviewText'], inplace=True)
-#df = df[:19700]
+# Apply preprocessing to the 'reviewText' column
+df['cleaned_reviewText'] = df['reviewText'].apply(preprocess_text)
+processed_df = df[['cleaned_reviewText']]
 
-# Apply preprocessing to reviewText
-df['processed_reviewText'] = df['reviewText'].apply(preprocess_text)
-processed_df = df[['processed_reviewText']]
 
-# Save the DataFrame to a new CSV file
-processed_df.to_csv('processed_reviews.csv', index=False)
+# Save the preprocessed data to a new CSV file
+processed_df.to_csv('preprocessed_reviews.csv', index=False)
+
