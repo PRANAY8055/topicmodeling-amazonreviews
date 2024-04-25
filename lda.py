83d91b52ebcb5d16e8c9d5f47202aa5a3b261b5a
Tanujk23 <115383226+Tanujk23@users.noreply.github.com>
2024-04-24 23:10:48 -0400

    Update lda.py

--- lda.py
+++ lda.py
@@ -8,21 +8,27 @@ Created on Wed Mar 27 18:09:42 2024
 import numpy as np
 import pandas as pd
 import nltk
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
 import re
 import gensim
+import operator
+import pprint
 from gensim import corpora, models
-
+from gensim.models.coherencemodel import CoherenceModel
 from sklearn.feature_extraction.text import CountVectorizer
+from collections import defaultdict
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+
 nltk.download('omw-1.4')
 nltk.download('stopwords')
 nltk.download('wordnet')
 
+# Preprocessing the Data
 stop_words = set(stopwords.words('english'))
 lemmatizer = WordNetLemmatizer()
 
-reviews_df = pd.read_csv(r'C:\Users\Sreekar\Downloads\electronics_sample.csv\electronics_sample.csv' )
+reviews_df = pd.read_csv(r'.\electronics_sample.csv' )
 if 'summary' in reviews_df.columns:
     df.drop('summary', axis=1, inplace=True)
 reviews_df.dropna(subset=['reviewText'], inplace=True)
@@ -65,8 +71,7 @@ corpus = [id2word.doc2bow(text) for text in tokenized_reviews]
 
 [[(id2word[id], freq) for id, freq in cp] for cp in corpus[:10]]
 
-from collections import defaultdict
-import operator
+
 
 word_freq = defaultdict(int)
 for document in corpus:
@@ -82,9 +87,13 @@ sorted_normalized_word_freq = sorted(normalized_word_freq.items(), key=operator.
 
 for word_id, normalized_freq in sorted_normalized_word_freq[:50]:
     print(f"{id2word[word_id]}: {normalized_freq}")
+
+# basic LDA Model
 ldamodel = models.LdaModel(corpus=corpus, id2word=id2word, num_topics=7, passes=10, random_state=42)
+
+# Tuned LDA Model with alpha and eta
 lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus, id2word=id2word, num_topics=7, passes=10, alpha='auto', eta='auto', random_state=42)    
-import pprint
+
 pprint.pprint(lda_model.print_topics())
 doc_lda = lda_model[corpus]
 
@@ -99,8 +108,8 @@ reviews_df['topics'] = [
 ]
 print(reviews_df[['reviewText', 'topics']].head())
 
-from gensim.models.coherencemodel import CoherenceModel
 
+# Calculating Coherence score for both the models
 coherence_model_lda_cv = CoherenceModel(model=ldamodel, texts=tokenized_reviews, dictionary=id2word, coherence='c_v')
 coherence_lda_cv = coherence_model_lda_cv.get_coherence()
 print('Basic LDA Coherence Score (C_v): ', coherence_lda_cv)
